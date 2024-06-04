cbbdc0af8a2b2bfa81e37bab1943927a5198dcfc
stopachka <stepan.p@gmail.com>
2024-06-04 12:32:43 -0400

    vanilla auth example

---
+++ README.md
@@ -0,0 +1,8 @@
+# auth-vanilla 
+
+This demos magic code auth powered by `@instantdb/core`, on vanilla vite.
+
+```bash
+pnpm i 
+pnpm run dev
+```
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,95 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { User, init } from "@instantdb/core";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const db = init({
+  appId: "5b640925-7328-451d-b79e-a7d4c8b48adb",
+});
+
+const appEl = document.querySelector<HTMLDivElement>("#app")!;
+
+function renderLoading() {
+  appEl.innerHTML = `<div>Loading...</div>`;
+}
+
+function renderAuthError(message: string) {
+  appEl.innerHTML = `<div>Uh oh! ${message}</div>`;
+}
+
+function renderLoggedInPage(user: User) {
+  appEl.innerHTML = `
+    <div>
+      <h1>Welcome, ${user.email}!</h1>
+      <button id='sign-out-button'>Sign out</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  `;
+  const signOutBtn =
+    document.querySelector<HTMLButtonElement>("#sign-out-button")!;
+  signOutBtn.addEventListener("click", () => {
+    db.auth.signOut();
+  });
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function renderSignInPage() {
+  appEl.innerHTML = `
+    <div>
+      <form id='email-input-form'>
+        <h3>Welcome. Let's log you in</h3>
+        <p>
+          Enter your email, and we’ll send you a verification code.
+          We'll create an account for you too if you don't already have one :)
+        </p>
+        <input type='email' name='email' placeholder='Email' />
+        <button type='submit'>Send code</button>
+      </form>
+    </div>
+  `;
+  const formEl = document.querySelector<HTMLFormElement>("#email-input-form")!;
+  formEl.email.focus();
+  formEl.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = formEl.email.value;
+    try {
+      await db.auth.sendMagicCode({ email });
+    } catch (e: any) {
+      alert(`Uh oh! ${e.body?.message}`);
+    }
+    renderMagicCodePage(email);
+  });
+}
+
+function renderMagicCodePage(email: string) {
+  appEl.innerHTML = `
+    <div>
+      <form id='magic-code-form'>
+        <h3>Check your email</h3>
+        <p>
+          We've sent a magic code to ${email}. Enter it below to sign in.
+        </p>
+        <input type='text' name='code' placeholder='Magic Code' />
+        <button type='submit'>Verify code</button>
+      </form>
+    </div>
+  `;
+  const formEl = document.querySelector<HTMLFormElement>("#magic-code-form")!;
+  formEl.code.focus();
+  formEl.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const code = formEl.code.value;
+    try {
+      db.auth.signInWithMagicCode({ email, code });
+    } catch (e: any) {
+      alert(`Uh oh! ${e.body?.message}`);
+    }
+  });
+}
+
+renderLoading();
+db.subscribeAuth((auth) => {
+  if (auth.error) {
+    renderAuthError(auth.error.message);
+  } else if (auth.user) {
+    renderLoggedInPage(auth.user);
+  } else {
+    renderSignInPage();
+  }
+});
